e82f1f582b394b83f112b6c71f2bd1baab01b19a
white weasel <whiteweasel@mail.ru>
2022-06-15 16:38:24 +0300

    Görüntü işleme yüz tanıma programı
    yüzümü kaydetiyoruz ve bir isim veriyoruz daha sonra tekrar karşısına cıktığında tanıyor ve isim gözüküyor

---
+++ FaceDetection-ImageProcessing/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FaceDetection_ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.start = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.İsim = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.Location = new System.Drawing.Point(32, 29);
+            this.cameraBox.Margin = new System.Windows.Forms.Padding(2);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(301, 311);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(416, 29);
+            this.start.Margin = new System.Windows.Forms.Padding(2);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(98, 40);
+            this.start.TabIndex = 3;
+            this.start.Text = "Yüz Tanımayı Başlat";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(416, 204);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(98, 44);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Yüzü Kaydet";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(416, 135);
+            this.textName.Margin = new System.Windows.Forms.Padding(2);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(120, 20);
+            this.textName.TabIndex = 5;
+            // 
+            // İsim
+            // 
+            this.İsim.AutoSize = true;
+            this.İsim.Location = new System.Drawing.Point(374, 135);
+            this.İsim.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.İsim.Name = "İsim";
+            this.İsim.Size = new System.Drawing.Size(25, 13);
+            this.İsim.TabIndex = 6;
+            this.İsim.Text = "İsim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.İsim);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.cameraBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Yüz tanıma";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox cameraBox;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label İsim;
+    }
+}
+
---
+++ FaceDetection-ImageProcessing/Form1.cs
@@ -0,0 +1,120 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FaceDetection_ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImage = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLables, t;
+        string name, names = null;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            //HaarCascade yüz tanıma için
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+                //İlk etiket kaydedilen yüzlerin sayısıdır.
+                NumLables = Convert.ToInt16(Labels[0]);
+                Count = NumLables;
+                string FacesLoad;
+                for(int i=1; i< NumLables+1; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    trainingImage.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("VeriTabanında bir şey yok");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(frameProcedure);
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Count = Count + 1;
+            grayFace = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] DetectedFaces = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in DetectedFaces[0])
+            {
+                TrainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
+                break;
+            }
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            trainingImage.Add(TrainedFace);
+            labels.Add(textName.Text);
+            File.WriteAllText(Application.StartupPath + "/Faces/Face.txt", trainingImage.ToArray().Length.ToString() + ",");
+            for (int i=1; i<trainingImage.ToArray().Length +1; i++)
+            {
+                trainingImage.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/Face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/Faces/Face.txt", labels.ToArray()[i - 1] + ",");
+
+            }
+            MessageBox.Show(textName.Text + "Ekleme Başarılı");
+        }
+        private void frameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach(MCvAvgComp f in facesDetectedNow[0])
+            {
+                result=Frame.Copy(f.rect).Convert<Gray,Byte>().Resize(100,100,Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if (trainingImage.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImage.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+               }
+               
+                Users.Add("");
+
+            }
+            cameraBox.Image = Frame;
+            names = "";
+            Users.Clear();
+
+        }
+    }
+}
